6629572c612a8230d300ecb9c7d73bf7c840052f
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2019-01-28 23:07:34 +0200

    Tested with one left

--- Robot2019-ArduinoLineFollower.ino
+++ Robot2019-ArduinoLineFollower.ino
@@ -6,43 +6,43 @@
 // // Left
 
 // Number of sensors used in the left side
-#define leftSensorCount   16     
+#define leftSensorCount 16     
 
 // Maximum timeout for sensors to be considered low in the left side
-#define leftSensorTimeout       2500
+#define leftSensorTimeout 2500
 
 // The 0th sensor in the left array
-#define left0Pin 3
+#define left0Pin 53
 // The 1st sensor in the left array
-#define left1Pin 4
+#define left1Pin 52
 // The 2nd sensor in the left array
-#define left2Pin 5
+#define left2Pin 51
 // The 3rd sensor in the left array
-#define left3Pin 6
+#define left3Pin 50
 // The 4th sensor in the left array
-#define left4Pin 7
+#define left4Pin 49
 // The 5th sensor in the left array
-#define left5Pin 8
+#define left5Pin 48
 // The 6th sensor in the left array
-#define left6Pin 9
+#define left6Pin 47
 // The 7th sensor in the left array
-#define left7Pin 10
+#define left7Pin 46
 // The 8th sensor in the left array
-#define left8Pin 11
+#define left8Pin 45
 // The 9th sensor in the left array
-#define left9Pin 12
+#define left9Pin 44
 // The 10th sensor in the left array
-#define left10Pin 13
+#define left10Pin 43
 // The 11th sensor in the left array
-#define left11Pin 14
+#define left11Pin 42
 // The 12th sensor in the left array
-#define left12Pin 15
+#define left12Pin 41
 // The 13th sensor in the left array
-#define left13Pin 16
+#define left13Pin 40
 // The 14th sensor in the left array
-#define left14Pin 17
+#define left14Pin 39
 // The 15th sensor in the left array
-#define left15Pin 18
+#define left15Pin 38
 
 // Define the left sensor (given the defined constants above)
 QTRSensorsRC leftSensor((unsigned char[]) {left0Pin, left1Pin, left2Pin, left3Pin, left4Pin, left5Pin, left6Pin, left7Pin, left8Pin, left9Pin, left10Pin, left11Pin, left12Pin, left13Pin, left14Pin, left15Pin},
@@ -53,10 +53,10 @@ unsigned int leftSensorValues[leftSensorCount];
 // // Right
 
 // Number of sensors used in the right side
-#define rightSensorCount   16     
+#define rightSensorCount 16     
 
 // Maximum timeout for sensors to be considered low in the right side
-#define rightSensorTimeout       2500
+#define rightSensorTimeout 2500
 
 // The 0th sensor in the right array
 #define right0Pin 3
@@ -92,8 +92,8 @@ unsigned int leftSensorValues[leftSensorCount];
 #define right15Pin 18
 
 // Define the right sensor (given the defined constants above)
-QTRSensorsRC rightSensor((unsigned char[]) {right0Pin, right1Pin, right2Pin, right3Pin, right4Pin, right5Pin, right6Pin, right7Pin, right8Pin, right9Pin, right10Pin, right11Pin, right12Pin, right13Pin, right14Pin, right15Pin},
-  rightSensorCount, rightSensorTimeout, QTR_NO_EMITTER_PIN);
+//QTRSensorsRC rightSensor((unsigned char[]) {right0Pin, right1Pin, right2Pin, right3Pin, right4Pin, right5Pin, right6Pin, right7Pin, right8Pin, right9Pin, right10Pin, right11Pin, right12Pin, right13Pin, right14Pin, right15Pin},
+//  rightSensorCount, rightSensorTimeout, QTR_NO_EMITTER_PIN);
 unsigned int rightSensorValues[rightSensorCount];
 
 // Begin communication with the roborio
